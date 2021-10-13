698e1f148c60a6a3296dd4d7001e342433969a37
WiseWraith <anoshk1n@mail.ru>
2021-10-13 17:26:52 +0300

    up

--- lab2.kt
+++ lab2.kt
@@ -3,59 +3,55 @@ open class Person(var name: String, var birthYear: Int) {
 }
 
 class Student(
-    S_name: String, S_birthYear: Int,
+    name_s: String, birthYear_s: Int,
     var averageGrade: Double,
     var extramural: Boolean = false
 ) :
-    Person(S_name, S_birthYear) {
+    Person(name_s, birthYear_s) {
 
 }
 
 class Lecturer(
-    L_name: String, L_birthYear: Int,
+    name_l: String, birthYear_l: Int,
     var degree: String,
     var experienceFrom: Int
 ) :
-    Person(L_name, L_birthYear) {
+    Person(name_l, birthYear_l) {
 
 }
 
 fun main() {
     var persons = listOf(
-        Student("Казантипов Олег", 2000, 4.3),
-        Student("Приемников Паша", 2001, 4.1),
-        Student("Растрворова Екатерина", 2000, 3.9, true),
-        Student("Морозов Даниил", 2001, 4.6),
-        Student("Отличников Антон", 2002, 5.0, true),
-        Lecturer("Панасенко Пётр", 1972, "Кандидат технических наук", 2001),
-        Lecturer("Агафонов Владимир", 1975, "Кандидат педагогических наук", 2004),
-        Lecturer("Липова Надежда", 1981, "Кандидат экономических наук", 2010),
-        Lecturer("Дебоширов Алексей", 1954, "Доктор технических наук", 1992),
-        Lecturer("Берёзова Татьяна", 1963, "Доктор филологических наук", 2001)
+        Student("Фамилия1 Имя1", 2000, 4.3),
+        Student("Фамилия2 Имя2", 2001, 4.1),
+        Student("Фамилия3 Имя3", 2000, 3.9, true),
+        Student("Фамилия4 Имя4", 2001, 4.6),
+        Student("Фамилия5 Имя5", 2002, 5.0, true),
+        Lecturer("Фамилия6 Имя6", 1972, "Профессор математических наук", 2001),
+        Lecturer("Фамилия7 Имя7", 1975, "Кандидат филологических наук", 2004),
+        Lecturer("Фамилия8 Имя8", 1981, "Кандидат экономических наук", 2010),
+        Lecturer("Фамилия9 Имя9", 1954, "Доктор технических наук", 1992),
+        Lecturer("Фамилия10 Имя10", 1963, "Кандидат  педагогических наук", 2001)
     )
-
     println("Сортировка списка Persons по возрасту в порядке убывания:")
     persons.sortByAge().forEach { println("Имя: ${it.name} | Возраст: ${it.age}") }
 
     println("\nСортировка Students по имени в порядке убывания:")
-    var persons_S = mutableListOf<Student>()
-    persons.forEach { if (it is Student) persons_S.add(it) }
-    persons_S.sortByNameStudents().forEach { println("Имя: ${it.name} | Возраст: ${it.age}") }
+    var persons_stud = mutableListOf<Student>()
+    persons.forEach { if (it is Student) persons_stud.add(it) }
+    persons_stud.sortByNameStudents().forEach { println("Имя: ${it.name} | Возраст: ${it.age}") }
 
     println("\nСортировка очников по средней оценке в порядке убывания")
-    persons_S.sortByAverageGrade(true).forEach { println("Имя: ${it.name} | Возраст: ${it.averageGrade}") }
+    persons_stud.sortByAverageGrade(true).forEach { println("Имя: ${it.name} | Возраст: ${it.averageGrade}") }
 
 }
 
-fun List<Person>.sortByAge(): List<Person> {
+fun List<Person>.sortByAge() = this.sortedByDescending { it.age }
 
-    return this.sortedByDescending { it.age }
-}
+fun List<Student>.sortByNameStudents(): List<Student> =
 
-fun List<Student>.sortByNameStudents(): List<Student> {
+    this.sortedByDescending { it.name }
 
-    return this.sortedByDescending { it.name }
-}
 
 fun List<Student>.sortByAverageGrade(exceptExtramural: Boolean): List<Student> {
 
